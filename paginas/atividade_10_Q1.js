e40c40a80f121f4025993936f450b25bcff19239
vinicius2129 <139911616+vinicius2129@users.noreply.github.com>
2023-10-23 21:02:50 -0300

    Add files via upload

---
+++ paginas/atividade_10_Q1.js
@@ -0,0 +1,6 @@
+function js_style() {
+    var p = document.getElementById('text');
+    p.style.fontFamily = 'Franklin Gothic Medium'; 
+    p.style.fontSize = '34px';
+    p.style.color = 'blue';
+}
